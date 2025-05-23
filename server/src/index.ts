f6b55beaa9d00f6c0d496b222848bfd380e8170b
Haridev <haridev.8281@outlook.com>
2025-05-23 15:52:09 +0000

    feat: update prisma schema

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import { toNodeHandler } from "better-auth/node";
 import { httpLogger, logger } from "./configs/pino.js";
+import { auth } from "./utils/auth.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(express.json());
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 app.use(httpLogger);
+app.use(express.json());
 
 app.listen(PORT, () => {
   logger.info(`Server is running on port ${PORT}`);
